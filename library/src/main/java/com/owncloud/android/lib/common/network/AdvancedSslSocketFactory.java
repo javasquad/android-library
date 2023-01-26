8c6788565bcb637f20a53c1d82c36a9d32c0842b
kushmittal2009 <76464198+kushmittal2009@users.noreply.github.com>
2023-01-25 17:25:20 -0800

    Update AdvancedSslSocketFactory.java
    Signed-off-by: kushmittal2009 <76464198+kushmittal2009@users.noreply.github.com>

--- library/src/main/java/com/owncloud/android/lib/common/network/AdvancedSslSocketFactory.java
+++ library/src/main/java/com/owncloud/android/lib/common/network/AdvancedSslSocketFactory.java
@@ -25,6 +25,8 @@
 package com.owncloud.android.lib.common.network;
 
 import com.nextcloud.common.DNSCache;
+import android.content.Context;
+import android.util.Log;
 import com.owncloud.android.lib.common.utils.Log_OC;
 
 import org.apache.commons.httpclient.ConnectTimeoutException;
@@ -49,7 +51,7 @@ import javax.net.ssl.SSLParameters;
 import javax.net.ssl.SSLPeerUnverifiedException;
 import javax.net.ssl.SSLSession;
 import javax.net.ssl.SSLSocket;
-
+import de.ritscher.ssl.InteractiveKeyManager;
 
 /**
  * AdvancedSSLProtocolSocketFactory allows to create SSL {@link Socket}s with
@@ -65,12 +67,13 @@ public class AdvancedSslSocketFactory implements SecureProtocolSocketFactory {
     private SSLContext mSslContext = null;
     private AdvancedX509TrustManager mTrustManager = null;
     private X509HostnameVerifier mHostnameVerifier = null;
-
+    private Context mContext = null;
     /**
      * Constructor for AdvancedSSLProtocolSocketFactory.
      */
     public AdvancedSslSocketFactory(
-            SSLContext sslContext, AdvancedX509TrustManager trustManager, X509HostnameVerifier hostnameVerifier
+      SSLContext sslContext, AdvancedX509TrustManager trustManager, X509HostnameVerifier hostnameVerifier,
+            Context context
     ) {
 
         if (sslContext == null)
@@ -83,6 +86,7 @@ public class AdvancedSslSocketFactory implements SecureProtocolSocketFactory {
         mSslContext = sslContext;
         mTrustManager = trustManager;
         mHostnameVerifier = hostnameVerifier;
+	mContext = context;
     }
 
     public SSLContext getSslContext() {
@@ -249,8 +253,14 @@ public class AdvancedSslSocketFactory implements SecureProtocolSocketFactory {
             ///	(that should be an instance of AdvancedX509TrustManager) 
             try {
                 SSLSocket sock = (SSLSocket) socket;    // a new SSLSession instance is created as a "side effect" 
-                sock.startHandshake();
-
+	    try {
+                    sock.startHandshake();
+                } catch (SSLHandshakeException e1) {
+                    Log.w(TAG, "SSL handshake failed; remove key chain aliases disabled", e1);
+                    //new InteractiveKeyManager(mContext).removeKeys(sock.getInetAddress().getHostName(), sock.getPort());
+                    throw e1;
+                }
+		    
             } catch (RuntimeException e) {
 
                 if (e instanceof CertificateCombinedException) {
